865480499980176ad809853a9981c21e2ac8a7f8
Anthony Vu <70167429+vua11@users.noreply.github.com>
2022-03-24 10:37:01 -0400

    Update Reverse Integer.py

--- Reverse Integer.py
+++ Reverse Integer.py
@@ -1,1 +1,4 @@
-
+class Solution:
+    def reverse(self, x: int) -> int:
+        rev_x = int("-" + str(x)[::-1][:-1]) if x < 0 else int(str(x)[::-1])
+        return 0 if rev_x.bit_length() >= 32 else rev_x
